763e0645f625b0343ba64ef1c459ec7576f75af9
AndrewValerio <andrewvalerio1396@gmail.com>
2022-06-08 16:05:27 -0700

    Fixed Show More bug

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 console.log("Page Loaded");
 
 const api_key = "RNtYFS9Q4vYDV1E5LLJsw58nQdlOZReC";
-const limit = 9;
+const limit = 3;
 const rating = 'g';
 let pageNumber = 0;
-let movePage = 0;
 
 
 const resultButton = document.querySelector("#clicker");
@@ -12,17 +11,18 @@ let resultSearch = document.querySelector("#result");
 const showMoreResults = document.querySelector("#showMore");
 
 let searchTerm = document.querySelector("#searchTerm");
+let prevSearch = "";
 
 
 async function getResults(){
     console.log("get results reached");
+    const offset = pageNumber * limit;
 
     console.log(searchTerm);
-    pageNumber += 1;
-    console.log("page number: " + pageNumber);
+    console.log("page number: " + (pageNumber + 1));
 
 
-    let apiUrl =`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchTerm.value}&limit=9&offset=${movePage}`;
+    let apiUrl =`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchTerm.value}&limit=${limit}&offset=${offset}`;
     console.log(apiUrl);
     const response = await fetch(apiUrl);
     const responseData = await response.json();
@@ -30,17 +30,19 @@ async function getResults(){
     console.log("response is: ", response);
     console.log("responseData is: ", responseData);
     console.log("data is: ", data)
-    
+    if(prevSearch != searchTerm.value){
+        pageNumber = 0;
+        resultSearch.innerHTML = "";
+        prevSearch = searchTerm;
+    }
     displayResults(data);
     showMoreResults.classList.remove("hidden");
-    showMoreResults.addEventListener('click', (e) => {
-        e.preventDefault()
-        showMore(data);});
+        pageNumber++;
+    prevSearch = searchTerm.value;
 }
 
 function displayResults(data){
     console.log(data[0].images.original.url);
-    resultSearch.innerHTML = "";
     data.forEach((gif,index) => {
         resultSearch.innerHTML += ` 
         <img src = "${data[index].images.original.url}" alt = "Images">
@@ -48,10 +50,7 @@ function displayResults(data){
     });
 }
 
-function showMore(data){
-    movePage = pageNumber * limit;
-    getResults();
-}
+
 
 //resultButton.addEventListener('click', (e) => {
    // e.preventDefault()
@@ -61,6 +60,9 @@ window.onload = function(){
     resultButton.addEventListener('click', (e) => {
         e.preventDefault()
         getResults();});
+        showMoreResults.addEventListener('click', (e) => {
+            e.preventDefault()
+            getResults();});
 }
 
 
